65afb01a3db3787b87a9c718f4122f31c32e8dc3
SaoHoServices <Services@SaoHo.la>
2021-12-21 16:41:18 +0800

    SaoHo

---
+++ SettingsExample.cpp
@@ -0,0 +1,46 @@
+// Copyright (C) 2021 MoonStudio, Inc. All Rights Reserved.
+
+
+#include "SettingsExample.h"
+
+#define SettingsExampleCFG "SettingsExample.ini"
+
+USettingsExample::USettingsExample(const FObjectInitializer &ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	CategoryName = "SaoHo";
+	SectionName = "SettingsExample";
+	bRestartable = true;
+
+	bGlobalSettings = true;
+	
+	ConfigTest = TEXT("SettingsExample");
+
+	EngineConfigFile = FPaths::EngineConfigDir() + SettingsExampleCFG;
+	ProjectConfigFile = FPaths::ProjectConfigDir() + SettingsExampleCFG;
+}
+
+USettingsExample &USettingsExample::Get()
+{
+	return *GetMutableDefault<USettingsExample>();
+}
+
+FString USettingsExample::GetConfigName() const
+{
+	//return Super::GetConfigName();
+	return bGlobalSettings ? EngineConfigFile : ProjectConfigFile;
+}
+
+// ==========================================================================
+//                                                        Events
+// ==========================================================================
+
+void USettingsExample::PreSaveSection()
+{
+	SetSection("ThemeStyle", "GlobalSettings", bGlobalSettings ? "True" : "False", EngineConfigFile);
+}
+
+void USettingsExample::PreLoadSection()
+{
+	bGlobalSettings = GetSection("ThemeStyle", "GlobalSettings", EngineConfigFile) == "True";
+}
